6761652fe9414c5737c8e63caf34088dc0339a74
Ilya Rovensky <ilya.rovensky@gmail.com>
2021-05-15 16:53:47 +0300

    event test rewrite

--- tests/SlikCord.IntegrationTests/EventServiceTests.cs
+++ tests/SlikCord.IntegrationTests/EventServiceTests.cs
@@ -19,57 +19,58 @@ namespace Slik.Cord.IntegrationTests
         private readonly Events.EventsClient _client = new(Channel);
         private const string TestTopic = "/test-topic";
 
-        private async Task SubscribeGetOrAssert()
+        private async Task SubscribeGetOrAssert(Func<Task> publishAction)
         {
-            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(3));
-
+            
             var subscribeRequest = new SubscribeRequest();
             subscribeRequest.Filters.Add($"topic=='{TestTopic}'");
             subscribeRequest.Filters.Add($"namespace=={ContainerdNamespace}");
 
             using var streamingCall = _client.Subscribe(subscribeRequest, Headers);
+
+            await publishAction();
             
-            await foreach (var _ in streamingCall.ResponseStream.ReadAllAsync(cts.Token))
+            await foreach (var _ in streamingCall.ResponseStream.ReadAllAsync())
             {
                 return;
             }
+
+            Assert.Fail();
         }
 
         [TestMethod]
         public async Task Subscribe_PublishedEvent_GetsEvent()
-        {
-            var subscribeTask = SubscribeGetOrAssert();
-
-            var request = new PublishRequest
+        {            
+            await SubscribeGetOrAssert(async () =>             
             {
-                Topic = TestTopic,
-                Event = Any.Pack(new Empty())
-            };
+                var request = new PublishRequest
+                {
+                    Topic = TestTopic,
+                    Event = Any.Pack(new Empty())
+                };
 
-            await _client.PublishAsync(request, Headers);
-            
-            await subscribeTask;
+                await _client.PublishAsync(request, Headers);
+            });
         }
 
         [TestMethod]
         public async Task Subscribe_ForwardedEvent_GetsEvent()
-        {            
-            var subscribeTask = SubscribeGetOrAssert();
-
-            var request = new ForwardRequest
+        {
+            await SubscribeGetOrAssert(async () =>
             {
-                Envelope = new Envelope
+                var request = new ForwardRequest
                 {
-                    Event = Any.Pack(new Empty()),
-                    Topic = TestTopic,
-                    Namespace = ContainerdNamespace,
-                    Timestamp = Timestamp.FromDateTime(DateTime.UtcNow)
-                }
-            };
+                    Envelope = new Envelope
+                    {
+                        Event = Any.Pack(new Empty()),
+                        Topic = TestTopic,
+                        Namespace = ContainerdNamespace,
+                        Timestamp = Timestamp.FromDateTime(DateTime.UtcNow)
+                    }
+                };
 
-            await _client.ForwardAsync(request, Headers);
-
-            await subscribeTask;
+                await _client.ForwardAsync(request, Headers);
+            });
         }
     }
 }
